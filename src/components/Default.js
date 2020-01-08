5544441e91cedf90f4e7e4e92d95af680e2952f8
Haja Andriamaro <haja.andri@gmail.com>
2020-01-08 11:10:00 +0100

    reorganised the routes and layers to be able to render the landing page on empty route and relevant components matching the routes

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,25 +3,28 @@ import { AppContext } from '../utils/AppProvider';
 import EmptyView from './EmptyView';
 import SnippetList from './List';
 import FuzzySearch from 'react-fuzzy';
+import MainLayout from "../views/layout";
 
 const Default = () => {
-  console.log("calling defaukl")
   const { state } = useContext(AppContext);
   const token =
     typeof window !== 'undefined' && window.localStorage.getItem('token');
 
-  //Render filterd Snippets if there are any
-  if (state.filteredSnippets) {
-    return <SnippetList data={state.filteredSnippets} />;
-  }
-
-  // Render the list of snippets if their are any
+  // Render the list of snippets if their are any and user has token
   if (state.snippetsData && state.snippetsData.length && token) {
-    return <SnippetList data={state.snippetsData} />;
+    return(
+      <MainLayout>
+        <SnippetList data={state.filteredSnippets} />
+      </MainLayout>
+    ); 
   }
   // default view if there is no snippets
   else {
-    return <EmptyView />;
+    return(
+      <MainLayout>
+        <EmptyView />
+      </MainLayout>
+    );
   }
 };
 
