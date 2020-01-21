6a6cd9b5177f00d066398bec94f99198379e3de5
Sorin Chis <kish.sorin@yahoo.com>
2020-01-21 20:08:41 +0200

    change to Slack button

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -104,7 +104,7 @@ function Landing() {
                       Request Access
                     </Button>
                     <RequestAccess isOpen={isOpen} onClose={onClose} />
-                    <GoogleButton />
+                    <SlackButton />
                   </Stack>
                 )}
               </Box>
