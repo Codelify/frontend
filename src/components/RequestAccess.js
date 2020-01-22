6af75b18ee34e8f6b709d735de071c3e38b74f96
Haja <haja.andri@gmail.com>
2020-01-22 10:19:31 +0100

    Added success message after successful submission

--- src/components/RequestAccess.js
+++ src/components/RequestAccess.js
@@ -14,16 +14,18 @@ import {
     ModalBody,
     ModalCloseButton,
     Button,
+    Alert,
+    AlertIcon
 } from "@chakra-ui/core";
 import config from '../utils/config'
 
 const RequestAccess = ({isOpen, onClose})=>{
-    const [formData, setFormData] = useState({});
+    const [ formData, setFormData ] = useState({});
+    const [ success, setSuccess ] = useState(false)
 
     const onSubmit = (event) => {
         event.preventDefault();
-        console.dir(formData)
-        axios({
+        const params = {
             method: 'post',
             url: `https://api.moosend.com/v3/subscribers/${config.mooSend.listId}/subscribe.json?apikey=${config.mooSend.apiKey}`,
             //user: {"anystring": config.mailchimp.apiKey},
@@ -35,37 +37,30 @@ const RequestAccess = ({isOpen, onClose})=>{
                 "Email": formData.email,
                 "Name": formData.fullname,
             }
-        })
+        };
+        axios(params)
         .then(function(response) {
-            console.log(response.data);
-            // console.log("status " + response.status);
-            // console.log("statusText " + response.statusText);
-            // console.log("headers " + response.headers);
-            // console.log("config " + response.config);
+            setSuccess(true)
         })  
         .catch(function (error) {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-        }
-        console.log(error.config);
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
         });
-    }
-
-
-
-    
+    }    
 
     const handleChange = ({ target: { name, value } }) => {
         setFormData(prevState => ({
@@ -82,41 +77,58 @@ const RequestAccess = ({isOpen, onClose})=>{
             <ModalContent py="20px" borderRadius="5px" >
                 <ModalCloseButton _focus={{ outline: "none" }}/>
                 <ModalBody>   
-            <FormControl isRequired>
-            <Stack spacing={4}>
-                <Box>
-                    <FormLabel htmlFor="fullname">Full name</FormLabel>
-                    <Input 
-                    name="fullname" 
-                    focusBorderColor="#319795" 
-                    id="fname" 
-                    placeholder="Dan Abramov" 
-                    onChange={handleChange}
-                    />              
-                </Box>
-                <Box>
-                <FormLabel htmlFor="email">Email address</FormLabel>
-                <Input 
-                    name="email"  
-                    focusBorderColor="#319795" 
-                    placeholder="dan.abramov@reactjs.com" 
-                    type="email" 
-                    id="email" 
-                    aria-describedby="email-helper-text" 
-                    onChange={handleChange}
-                />
-                <FormHelperText id="email-helper-text">
-                    We'll never share your email.
-                </FormHelperText>      
-                </Box>
-            </Stack>
-            </FormControl>
-            </ModalBody>
+                    {
+                        success ? (
+                            <Alert borderRadius="5px" mt="40px" status="success">
+                            <AlertIcon />
+                                Your request for access has been recorder. Stay tuned!
+                            </Alert>                        
+                        ) :
+                        (
+                            <FormControl isRequired>
+                            <Stack spacing={4}>
+                                <Box>
+                                    <FormLabel htmlFor="fullname">Full name</FormLabel>
+                                    <Input 
+                                    name="fullname" 
+                                    focusBorderColor="#319795" 
+                                    id="fname" 
+                                    placeholder="Dan Abramov" 
+                                    onChange={handleChange}
+                                    />              
+                                </Box>
+                                <Box>
+                                <FormLabel htmlFor="email">Email address</FormLabel>
+                                <Input 
+                                    name="email"  
+                                    focusBorderColor="#319795" 
+                                    placeholder="dan.abramov@reactjs.com" 
+                                    type="email" 
+                                    id="email" 
+                                    aria-describedby="email-helper-text" 
+                                    onChange={handleChange}
+                                />
+                                <FormHelperText id="email-helper-text">
+                                    We'll never share your email.
+                                </FormHelperText>      
+                                </Box>
+                            </Stack>
+                            </FormControl>        
+                        )
+                    }
+                </ModalBody>
             <ModalFooter>
-                <Button _focus={{ outline: "none" }} variantColor="teal" mr={3} type="submit">
-                    Add me to the list
-                </Button>
-                <Button _focus={{ outline: "none" }} onClick={onClose}>Cancel</Button>
+                {
+                    !success &&
+                    (
+                        <>
+                        <Button _focus={{ outline: "none" }} variantColor="teal" mr={3} type="submit">
+                        Add me to the list
+                        </Button>
+                        <Button _focus={{ outline: "none" }} onClick={onClose}>Cancel</Button>    
+                        </>
+                    )
+                }
                 </ModalFooter>
             </ModalContent>
         </form>
