66b670b9d3b049a4b6f7821f39112755cc3171e6
Haja <haja.andri@gmail.com>
2020-01-22 10:39:14 +0100

    Handling form submission error case

--- src/components/RequestAccess.js
+++ src/components/RequestAccess.js
@@ -21,7 +21,8 @@ import config from '../utils/config'
 
 const RequestAccess = ({isOpen, onClose})=>{
     const [ formData, setFormData ] = useState({});
-    const [ success, setSuccess ] = useState(false)
+    const [ success, setSuccess ] = useState(false);
+    const [ error, setError ] = useState(false);
 
     const onSubmit = (event) => {
         event.preventDefault();
@@ -40,25 +41,15 @@ const RequestAccess = ({isOpen, onClose})=>{
         };
         axios(params)
         .then(function(response) {
-            setSuccess(true)
+            if(response.data.Error){
+                setError(true)
+            }
+            else setSuccess(true)
         })  
         .catch(function (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                // http.ClientRequest in node.js
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log('Error', error.message);
+            if(error.response || error.request ) {
+                setError(true)
             }
-            console.log(error.config);
         });
     }    
 
@@ -78,47 +69,59 @@ const RequestAccess = ({isOpen, onClose})=>{
                 <ModalCloseButton _focus={{ outline: "none" }}/>
                 <ModalBody>   
                     {
-                        success ? (
-                            <Alert borderRadius="5px" mt="40px" status="success">
+                        error ? (
+                            <Alert borderRadius="5px" mt="40px" status="error">
                             <AlertIcon />
-                                Your request for access has been recorder. Stay tuned!
-                            </Alert>                        
+                                There was an error processing your request.<br/>
+                                Please try again later.
+                            </Alert>
                         ) :
                         (
-                            <FormControl isRequired>
-                            <Stack spacing={4}>
-                                <Box>
-                                    <FormLabel htmlFor="fullname">Full name</FormLabel>
+                            success ? (
+                                <Alert borderRadius="5px" mt="40px" status="success">
+                                <AlertIcon />
+                                    Your request for access has been recorder. Stay tuned!
+                                </Alert>                        
+                            ) :
+                            (
+                                <FormControl isRequired>
+                                <Stack spacing={4}>
+                                    <Box>
+                                        <FormLabel htmlFor="fullname">Full name</FormLabel>
+                                        <Input 
+                                        name="fullname" 
+                                        focusBorderColor="#319795" 
+                                        id="fname" 
+                                        placeholder="Dan Abramov" 
+                                        onChange={handleChange}
+                                        />              
+                                    </Box>
+                                    <Box>
+                                    <FormLabel htmlFor="email">Email address</FormLabel>
                                     <Input 
-                                    name="fullname" 
-                                    focusBorderColor="#319795" 
-                                    id="fname" 
-                                    placeholder="Dan Abramov" 
-                                    onChange={handleChange}
-                                    />              
-                                </Box>
-                                <Box>
-                                <FormLabel htmlFor="email">Email address</FormLabel>
-                                <Input 
-                                    name="email"  
-                                    focusBorderColor="#319795" 
-                                    placeholder="dan.abramov@reactjs.com" 
-                                    type="email" 
-                                    id="email" 
-                                    aria-describedby="email-helper-text" 
-                                    onChange={handleChange}
-                                />
-                                <FormHelperText id="email-helper-text">
-                                    We'll never share your email.
-                                </FormHelperText>      
-                                </Box>
-                            </Stack>
-                            </FormControl>        
+                                        name="email"  
+                                        focusBorderColor="#319795" 
+                                        placeholder="dan.abramov@reactjs.com" 
+                                        type="email" 
+                                        id="email" 
+                                        aria-describedby="email-helper-text" 
+                                        onChange={handleChange}
+                                    />
+                                    <FormHelperText id="email-helper-text">
+                                        We'll never share your email.
+                                    </FormHelperText>      
+                                    </Box>
+                                </Stack>
+                                </FormControl>        
+                            )    
                         )
                     }
                 </ModalBody>
             <ModalFooter>
                 {
+                    error ? (
+                        <Button _focus={{ outline: "none" }} onClick={onClose}>Close</Button>
+                    ) :
                     !success &&
                     (
                         <>
