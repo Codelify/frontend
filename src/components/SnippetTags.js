c237ee678195d29d1d77f4b09d80895bf4269b2c
Haja <haja.andri@gmail.com>
2020-01-23 21:55:53 +0100

    Hamdling adding tags from list view (UI)

--- src/components/SnippetTags.js
+++ src/components/SnippetTags.js
@@ -19,26 +19,61 @@ const SnippetTags = ({
     styledEdit,
     handleUpdate
 }) => {
-    const handleBlur = event => {
-    //document.getElementById(event.target.id).classList.remove("edited-div");
-    handleToggle(false);
+
+  const [tagsList, setTagsList] = useState(tags);
+  
+  const tagsId = `tags_${id}`;
+  const handleBlur = event => {
+      handleToggle(false);
     };
 
     const [show, setShow] = useState(false);
 
     const handleToggle = newShow => {
-        setShow(newShow);
+      if(newShow){
+        console.log("get focus on " + tagsId);
+        document.getElementById(tagsId).focus();
+      }
+      setShow(newShow);
     };
 
-    const tagsId = `tags_${id}`;
+  // specfifid function to managed entered tags
+  const handleAddTags = event => {
+    let newTag = false;
+    let tag = event.target.value;
+    if (tag.charAt(tag.length - 1) === ",") {
+      // remove the comma
+      tag = tag.substring(0, tag.length - 1);
+      newTag = true;
+    }
+    if (( (event.key === "Enter" || event.key === "Tab") && event.target.value !== "") || newTag) {
+      // add it to the state holding the list of tags
+      setTagsList(prevState => [...prevState, tag]);
+      // clear the value held in the input field
+      event.target.value = "";
+    }
+  };    
+
+  // the Tab must be detected on key down 
+  // otherwise it cannot be captured in key up
+  const handleTab = event => {
+    let tag = event.target.value;
+    if(event.key === 'Tab' && tag !== ""){
+      // add it to the state holding the list of tags
+      setTagsList(prevState => [...prevState, tag]);
+      // clear the value held in the input field
+      event.target.value = "";
+    }
+  }
+
+
     return (
         <>
           <Stack justify="flex-start" flexWrap="wrap" isInline>
-            {tags &&
-              tags.map((tag, index) => {
+            {tagsList &&
+              tagsList.map((tag, index) => {
                 return (
                   <Tag
-                  id={tagsId}
                   key={index}
                   variant="subtle"
                   variantColor="teal"
@@ -62,7 +97,7 @@ const SnippetTags = ({
               })}
             <IconButton
             my="3px"
-            variant="ghost"
+            variant="outline"
             variantColor="teal"
             aria-label="Add a Tag"
             size="sm"
@@ -77,17 +112,16 @@ const SnippetTags = ({
           />
           </Stack>
 
-        <Collapse mt={0} isOpen={show}>
+        <Collapse isOpen={show}>
         <Input
-            id="tags"
+            id={tagsId}
             placeholder="Add tags (Press Enter or Comma for multiple tags)"
             focusBorderColor="#319795"
-            borderColor="#319795"
-            borderWidth="2px"
             name="tags"
             my="5px"          
-            // onKeyDown={handleTab}
-            // onKeyUp={handleAddTags}
+            onBlur={handleBlur}
+            onKeyDown={handleTab}
+            onKeyUp={handleAddTags}
           />
         </Collapse>
         </>
