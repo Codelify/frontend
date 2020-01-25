1c5373cc9db176b3b9107272fef459bdf1fd4cb2
Sorin Chis <kish.sorin@yahoo.com>
2020-01-25 16:09:36 +0200

    init ga

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Default from "./components/Default";
 import Login from "./components/Login";
 import Landing from "./views/Landing";
 import { Router } from "@reach/router";
 import SlackAuthenticator from "./components/SlackAuthenticator";
-import {} from "./components/~common/Tracking";
+import { initGA, PageView } from "./components/~common/Tracking";
 
 function App() {
+  useEffect(() => {
+    initGA("UA-156941509-1");
+    PageView();
+  }, []);
   return (
     <Router>
       <Landing path="/" />
