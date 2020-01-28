6c0c09b5581a7bf2d7f5f31dde639de3d1960142
Sorin Chis <kish.sorin@yahoo.com>
2020-01-28 18:18:54 +0200

    cleaning EmptyView Component

--- src/components/EmptyView.js
+++ src/components/EmptyView.js
@@ -1,23 +1,9 @@
 import React from "react";
-import { Text, Box, IconButton, useDisclosure } from "@chakra-ui/core";
-import NewSnippet from "./NewSnippet";
-import { MdAddCircle } from "react-icons/md";
-import { FaRegSadTear } from "react-icons/fa";
 import MainLayout from "../views/layout";
 import Spinner from "./~common/Spinner";
 import NoSnippetView from "./NoSnippetsView";
 
 const EmptyView = ({ loading }) => {
-  const [size, setSize] = React.useState("md");
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const firstField = React.useRef();
-
-  const handleClick = newSize => {
-    setSize(newSize);
-    onOpen();
-    document.getElementById("FiHome").focus();
-  };
-
   return (
     <MainLayout>
       {loading ? <Spinner /> : <NoSnippetView loading={loading} />}
