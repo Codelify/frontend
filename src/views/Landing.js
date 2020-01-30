473f845046aa9154ad213fdc3356172a7a0fd227
Sorin Chis <kish.sorin@yahoo.com>
2020-01-30 19:23:19 +0200

    added slack button back

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -113,7 +113,7 @@ function Landing() {
                       Request Access
                     </Button>
                     <RequestAccess isOpen={isOpen} onClose={onClose} />
-                    <GoogleButton />
+                    <SlackButton />
                   </Stack>
                 )}
               </Box>
