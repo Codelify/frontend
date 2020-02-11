d1c823d83a3c551087374bda58fe7f7b48e08383
Haja <haja.andri@gmail.com>
2020-02-11 14:50:07 +0100

    Functional access denied view

--- src/views/AccessDenied.js
+++ src/views/AccessDenied.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { Event, PageView, initGA } from "../components/~common/Tracking";
-import config from "../utils/config";
+import React from "react";
 import {
     ThemeProvider,
     CSSReset,
@@ -8,43 +6,26 @@ import {
     Heading,
     Text,
     Button,
-    Image,
     useDisclosure,
-    useColorMode,
-    Stack
 } from "@chakra-ui/core";
-import Header from "../components/Header";
+import { navigate } from "@reach/router";
 import Container from "../components/Container";
 import RequestAccess from "../components/RequestAccess";
-import screenShotLight from "../assets/img/app-shot-light.png";
-import screenShotDark from "../assets/img/app-shot-dark.png";
-import SlackButton from "../components/SlackButton";
-import { handleRouteChange } from "../utils/handleRouteChange";
-import { GiMinotaur } from 'react-icons/gi'
+import { GiMinotaur } from 'react-icons/gi';
+import { IoIosArrowBack } from 'react-icons/io'
 
 
-function Landing() {
-const [isLoggedIn, setIsLoggedIn] = useState(false);
+function AccessDenied() {
 const { isOpen, onOpen, onClose } = useDisclosure();
-const { colorMode } = useColorMode();
-
-useEffect(() => {
-    initGA(config.googleAnalytics.apiKey);
-    PageView();
-    if (typeof window !== "undefined" && window.localStorage.getItem("token")) {
-    setIsLoggedIn(true);
-    } else setIsLoggedIn(false);
-}, [isLoggedIn]);
 
 return (
     <ThemeProvider>
     <CSSReset />
     <Box mb={20}>
         <Box as="section" pt={40} pb={50}>
-        <Header  landing={true} isLoggedIn={isLoggedIn} />
         <Container>
             <Box maxW="xl" mx="auto" px="10px" textAlign="center">
-            <Box m="auto" as={GiMinotaur} size="64px" color="#E53E3E" />
+            <Box m="auto" as={GiMinotaur} size="64px" color="#F56565" />
             <Heading
                 as="h1"
                 size="xl"
@@ -53,13 +34,37 @@ return (
                 Event("Test Category", "Test Action", "Test Label")
                 }
             >
-                Restricted access
+                Restricted Access
             </Heading>
             <Text opacity="0.7" fontSize="lg" mt="6">
-                Access to Codelify is currently restricted to some users.
-                You can request an access using the form below to be added on our waiting list
+                Access to Codelify is currently restricted to some users only.
+                You can request an access to be added on our waiting list
             </Text>
 
+            <Box mt="6" >
+                
+                <Button 
+                    leftIcon={IoIosArrowBack}
+                    variant="ghost"
+                    size="md"
+                    mr="3px"
+                    onClick={() => {navigate('/')}}
+                    variantColor="teal"
+                    _focus={{ outline: "none" }}
+                    >
+                    Back
+                </Button>                
+                <Button
+                ml="3px"
+                variantColor="teal"
+                size="lg"
+                _focus={{ outline: "none" }}
+                onClick={onOpen}
+                >
+                Request Access
+                </Button>
+                <RequestAccess isOpen={isOpen} onClose={onClose} />
+            </Box>
             </Box>
         </Container>
         </Box>
@@ -69,4 +74,4 @@ return (
 );
 }
 
-export default Landing;
+export default AccessDenied;
