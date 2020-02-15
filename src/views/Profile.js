39c268d1231c8cd6d5cc19dcf184586b8d540cdb
Haja <haja.andri@gmail.com>
2020-02-15 22:16:41 +0100

    Final view formatting

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -16,12 +16,15 @@ import {
     InputRightElement,
     InputGroup,
     Divider,
+    Flex,
+    IconButton,
 } from "@chakra-ui/core";
+import ContentEditable from "react-contenteditable";
 import { navigate } from "@reach/router";
 import Container from "../components/Container";
 import { IoIosArrowBack } from "react-icons/io";
 import useUserData from "../components/~common/useUserData";
-import { FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { FaTwitter, FaLinkedin, FaEdit } from 'react-icons/fa';
 import { MdMail } from 'react-icons/md';
 import { MdVpnKey } from 'react-icons/md'
 
@@ -32,6 +35,7 @@ function Profile() {
     const { colorMode } = useColorMode();
     const [show, setShow] = React.useState(false);
     const handleClick = () => setShow(!show);    
+    const description = 'This my sweet and short Bio. Few interesting things about me, or things I am interested at.'
 
     return (
         <ThemeProvider>
@@ -53,7 +57,21 @@ function Profile() {
                 maxW="1280px" m="auto">
 
             <Box as="section" pt={40} pb={50}>
-            <Container>
+            <Container
+                backgroundColor={
+                    colorMode === "light" ? "#FAFAFA" : "rgba(45,55,72, 0.1)"
+                } 
+                p={10}
+            >
+                <Flex justifyContent="flex-end">
+                <IconButton
+                    variant="ghost"
+                    variantColor="teal"
+                    aria-label="Call Sage"
+                    fontSize="20px"
+                    icon={FaEdit}
+                />
+                </Flex>
                 <Box mb="40px" maxW="xl" mx="auto" px="10px" textAlign="center">
                 <Avatar
                     backgroundColor="none"
@@ -68,21 +86,28 @@ function Profile() {
                     m="20px"
                     size="xl"
                     fontWeight="bold"
-                    onClick={() =>
-                    Event("Test Category", "Test Action", "Test Label")
-                    }
                 >
                     Haja Andriamaro
                 </Heading>
-                <Text opacity="0.7" fontSize="lg" mt="6">
+                {/* <Text opacity="0.7" fontSize="lg" mt="6">
                     This my sweet and short Bio. Few interesting things about me, or
                     things I am interested at.
-                </Text>
+                </Text> */}
+
+                <Text opacity="0.5" as="div" mb="5px" contenteditable="true" fontSize="md">
+                <ContentEditable
+                    html={description}
+                    disabled={false}
+                    style={{
+                    outline: "none"
+                    }}
+                />
+                </Text>                
                 </Box>
                 <Box
                 borderRadius="10px"
                 backgroundColor={
-                    colorMode === "light" ? "#FAFAFA" : "rgba(45,55,72, 0.1)"
+                    colorMode === "light" ? "#FFFFFF" : "rgba(45,55,72, 0.1)"
                 } 
                 maxW="800px"
                 p="20px"
@@ -90,48 +115,66 @@ function Profile() {
                 m="auto"
                 >
                 <FormControl w="100%" isRequired={false}>
-                    <Stack justifyContent="space-between" w="100%" isInline spacing={4}>
+                    <Stack w="100%" spacing={4}>
                     <Box minW="300px">
-                        <FormLabel fontSize="xs" opacity="0.4" htmlFor="email">EMAIL ADDRESS</FormLabel>
+                        <FormLabel fontSize="xs" htmlFor="email">EMAIL ADDRESS</FormLabel>
                         <InputGroup>
-                        <InputLeftElement children={<Box as={MdMail} color="teal.400" />} />
-                        <Input type="email" value="haja.andri@gmail.com" focusBorderColor="#319795" />
+                            <InputLeftElement children={<Box as={MdMail} color="teal.400" />} />
+                            <Input 
+                            background="none" 
+                            disabled={true} 
+                            borderWidth="0px" 
+                            type="email" 
+                            value="haja.andri@gmail.com" 
+                            focusBorderColor="teal" 
+                            />
                         </InputGroup>
                     </Box>
-                    <Box minW="300px">
-                        <FormLabel fontSize="xs" opacity="0.4" htmlFor="fullname">PASSWORD</FormLabel>
-                        <InputGroup size="md">
-                        <InputLeftElement children={<Box as={MdVpnKey} color="teal.500" />} />
+                    <Box w="50%" minW="300px">
+                        <FormLabel fontSize="xs" htmlFor="fullname">PASSWORD</FormLabel>
+                        <Button h="1.75rem" size="sm" onClick={handleClick}>
+                                {show ? "Hide" : "Show"}
+                        </Button>
+                        <InputGroup>
+                        <InputLeftElement children={<Box as={MdVpnKey} color="teal.400" />} />
                         <Input
-                            pr="4.5rem"
+                            disabled
+                            background="none"
+                            borderWidth="0px"
                             type={show ? "text" : "password"}
                             value="mypassword"
                             focusBorderColor="teal.300"
                         />
-                        <InputRightElement width="4.5rem">
-                            <Button h="1.75rem" size="sm" onClick={handleClick}>
-                            {show ? "Hide" : "Show"}
-                            </Button>
-                        </InputRightElement>
                         </InputGroup>
-
                     </Box>
                     </Stack>
                 </FormControl>
                 <Divider my="30px" />
                 <Stack justifyContent="space-between" w="100%" spacing={4}>
                     <Box w="50%" minW="300px">
-                        <FormLabel fontSize="xs" opacity="0.4" htmlFor="email">TWITTER</FormLabel>
+                        <FormLabel fontSize="xs" htmlFor="email">TWITTER</FormLabel>
                         <InputGroup>
                         <InputLeftElement children={<Box as={FaTwitter} color="teal.400" />} />
-                        <Input placeholder="Phone number" focusBorderColor="#319795" />
+                        <Input 
+                        background="none" 
+                        disabled={true} 
+                        borderWidth="0px" 
+                        value="https://twitter.com/haja_andriam" 
+                        focusBorderColor="#319795" 
+                        />
                         </InputGroup>
                     </Box>
                     <Box w="50%" minW="300px">
-                        <FormLabel fontSize="xs" opacity="0.4" htmlFor="email">LINKEDIN</FormLabel>
+                        <FormLabel fontSize="xs" htmlFor="email">LINKEDIN</FormLabel>
                         <InputGroup>
                         <InputLeftElement children={<Box as={FaLinkedin} color="teal.400" />} />
-                        <Input placeholder="Phone number" focusBorderColor="teal.500" />
+                        <Input 
+                        background="none" 
+                        disabled={true} 
+                        borderWidth="0px" 
+                        value="https://www.linkedin.com/in/haja-andriamaro/" 
+                        focusBorderColor="teal.500" 
+                        />
                         </InputGroup>
                     </Box>
                 </Stack>
