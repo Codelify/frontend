2a4f785d441d72113f0e93a097d6611fc165adc4
Haja <haja.andri@gmail.com>
2020-03-02 14:43:26 +0100

    Changed logo to svg

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Image, Box } from "@chakra-ui/core";
-import mainLogo from "../assets/img/logo-green-grad.png";
+import mainLogo from "../assets/img/logo-green-grad.svg";
 
 const Logo = props => {
   return (
     <Box>
-      <Image size="150px" height="auto" src={mainLogo} alt="Codelify Logo" />
+      <Image width={["100px", "120px", "120px", "120px"]} height="auto" src={mainLogo} alt="Codelify Logo" />
     </Box>
   );
 };
