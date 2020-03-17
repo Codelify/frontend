7853f87177e5449e6af8b5f6c7b9a50687aa2db0
Haja <haja.andri@gmail.com>
2020-03-17 19:06:04 +0100

    finale version of share options UI

--- src/components/ShareOptions.js
+++ src/components/ShareOptions.js
@@ -6,8 +6,9 @@ import {
     Text,
     Stack,
     useClipboard,
+    Divider,
 } from '@chakra-ui/core';
-import { FaLink, FaEye, FaEyeSlash } from 'react-icons/fa'
+import { FaLink, FaEye, FaEyeSlash, FaTwitter } from 'react-icons/fa'
 
 const ShareOptions = () => {
     const [snippetPublicLink, setSnippetPublicLink] = useState("https://codelify.dev/snippets/hddeiHD739!hfii6gsU4");
@@ -19,16 +20,21 @@ const ShareOptions = () => {
 
     return(
         <>
-        <Stack py="10px" mt="10px" alignItems="center" justifyContent="flex-start" isInline>
+        <Divider p="20px" />
+        <Stack py="10px" alignItems="center" justifyContent="flex-start" isInline>
             <Box mx="5px" as={FaLink} />
             <Text fontSize="sm">{snippetPublicLink}</Text>
-            <Button onClick={onCopy} variantColor="teal" size="xs" _focus={{ outline: "none" }}>
+            <Button onClick={onCopy} variantColor="gray" size="xs" _focus={{ outline: "none" }}>
                 {hasCopied ? "Copied" : "Copy"}
             </Button>
         </Stack>
         <Stack py="10px" alignItems="center" justifyContent="flex-start" isInline>
-            <Box mx="5px" as={isPublic ? FaEye : FaEyeSlash } />
-            <Text fontSize="sm">This snippet is </Text> <Badge style={{cursor:"pointer"}} onClick={toggleVisibility} variantColor={isPublic ? "green" : "red"}>{isPublic ? "Public" : "Private"}</Badge>
+            <Box mx="5px" as={FaTwitter} />
+            <Text fontSize="sm">Push on Twitter</Text>
+        </Stack>
+        <Stack py="10px" alignItems="center" justifyContent="flex-start" isInline>
+            <Box mx="5px" as={isPublic ? FaEye : FaEyeSlash } color={isPublic ? "green.300" : "red.200"} />
+            <Badge style={{cursor:"pointer"}} onClick={toggleVisibility} variantColor={isPublic ? "green" : "red"}>{isPublic ? "Public" : "Private"}</Badge>
         </Stack>
         </>
     )
