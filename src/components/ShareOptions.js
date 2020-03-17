24801b3f3f79776fe93d44538a18f0b896a307c0
Haja <haja.andri@gmail.com>
2020-03-17 18:24:27 +0100

    toggle to make snippet public vs private

--- src/components/ShareOptions.js
+++ src/components/ShareOptions.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
+    Badge,
     Box,
     Button,
     Text,
     Stack,
     useClipboard,
 } from '@chakra-ui/core';
-import { FaLink } from 'react-icons/fa'
+import { FaLink, FaEye, FaEyeSlash } from 'react-icons/fa'
 
 const ShareOptions = () => {
-    const [snippetPublicLink, setSnippetPublicLink] = React.useState("https://codelify.dev/snippets/hddeiHD739!hfii6gsU4");
+    const [snippetPublicLink, setSnippetPublicLink] = useState("https://codelify.dev/snippets/hddeiHD739!hfii6gsU4");
+    const [isPublic, setIsPublic] = useState(false)
     const { onCopy, hasCopied } = useClipboard(snippetPublicLink);
+    const toggleVisibility = () => {
+        setIsPublic(!isPublic)
+    }
 
     return(
-        <Stack py="10px" my="20px" borderWidth="1px" alignItems="center" justifyContent="flex-start" isInline>
+        <>
+        <Stack py="10px" mt="10px" alignItems="center" justifyContent="flex-start" isInline>
             <Box mx="5px" as={FaLink} />
             <Text fontSize="sm">{snippetPublicLink}</Text>
             <Button onClick={onCopy} variantColor="teal" size="xs" _focus={{ outline: "none" }}>
                 {hasCopied ? "Copied" : "Copy"}
             </Button>
         </Stack>
+        <Stack py="10px" alignItems="center" justifyContent="flex-start" isInline>
+            <Box mx="5px" as={isPublic ? FaEye : FaEyeSlash } />
+            <Text fontSize="sm">This snippet is </Text> <Badge style={{cursor:"pointer"}} onClick={toggleVisibility} variantColor={isPublic ? "green" : "red"}>{isPublic ? "Public" : "Private"}</Badge>
+        </Stack>
+        </>
     )
 }
 
