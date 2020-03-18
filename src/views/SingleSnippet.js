d7ee1a3e9cc930b4347ebabbd2bc917b50cbf244
Haja <haja.andri@gmail.com>
2020-03-18 12:17:37 +0100

    initial structure bloc for Single Snippet

--- src/views/SingleSnippet.js
+++ src/views/SingleSnippet.js
@@ -1,25 +1,27 @@
-import { Box, Flex } from "@chakra-ui/core";
+import { Box, Flex, useColorMode } from "@chakra-ui/core";
 import React from "react";
-import Header from "../components/Header";
-import SideNav from "../components/SideNav";
+// import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Container from "../components/Container";
+import CodeSnippet from "../components/CodeSnippet";
 
-const Main = props => <Box as="main" mb="3rem" {...props} />;
 
-const MainLayout = ({ children }) => {
+const MainLayout = () => {
+const { colorMode } = useColorMode();
 return (
 <Box>
-    <Container>
-    <Header />
-    </Container>
-    <Flex align="center" justify="center" w="100%">
-    <Container d="flex">
-        <SideNav />
-        <Main ml={["0px", "0px", "210px", "210px"]} px="10px" w="100%" pt={8}>
-        {children}
-        </Main>
-    </Container>
+    <Flex align="center" justifyContent="center" w="100%">
+        <Box
+        w="90%"
+        px={["10px", "10px", "10px", "20px"]}
+        borderRadius="10px"
+        backgroundColor={
+            colorMode === "light" ? "#FAFAFA" : "rgba(45,55,72, 0.1)"
+        }
+        mt="50px"
+        py="40px"
+        >
+            <CodeSnippet />
+        </Box>
     </Flex>
     <Footer />
 </Box>
