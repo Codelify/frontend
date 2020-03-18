5d2ef215e8f12e0cc8a868f8eb05daa23dae66c6
Haja <haja.andri@gmail.com>
2020-03-18 16:06:37 +0100

    set some temp data to render the single snippet

--- src/views/SingleSnippet.js
+++ src/views/SingleSnippet.js
@@ -10,6 +10,34 @@ import UserBio from '../components/UserBio'
 import Footer from "../components/Footer";
 import CodeSnippet from "../components/CodeSnippet";
 
+const snippetPlaceHolder = `
+// Using useState hook to store the component state (form data)
+const [ formData, setFormData ] = useState({});
+
+// This part handle the state update based on the form data values
+// It receive the target element name and value
+const handleChange = ({ target: { name, value } }) => {
+  setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+  }));
+};
+
+// The field calling handleChange using onChange event
+<Input name="fullname" placeholder="Dan Abramov" onChange={handleChange} />
+    `;
+
+const snippet = {
+    title: "Single snippet Title",
+    id: "ID",
+    description: "When starting an update on the wrong branch, the following sequence allow to leave the branch clean, while moving all changes to the new (correct) branch",
+    url: "https://external-linkk.com",
+    tags: ["tag1", "tag2", "tag3"],
+    content: snippetPlaceHolder,
+    isFav: false,
+    index: 0
+    }
+
 
 const MainLayout = () => {
 const { colorMode } = useColorMode();
@@ -29,7 +57,19 @@ return (
         mt="50px"
         py="40px"
         >
-            <CodeSnippet />
+        <CodeSnippet
+            editMode={false}
+            index={snippet.index}
+            key={snippet.id}
+            id={snippet.id}
+            title={snippet.title}
+            description={snippet.description}
+            content={snippet.content}
+            tags={snippet.tags}
+            url={snippet.sourceUrl}
+            isFav={snippet.isFav}
+            isArchived={snippet.archivedAt}
+        />
         </Box>
     </Flex>
     <Footer />
