140633dcd8b1795f59e45477d6c5dfabc444e7d0
Haja <haja.andri@gmail.com>
2020-03-20 13:45:25 +0100

    refactored landing to use auth utils for authentication

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -20,27 +20,22 @@ import Footer from "../components/Footer";
 import Feature from '../components/Features'
 import Container from "../components/Container";
 import RequestAccess from "../components/RequestAccess";
-//import GoogleButton from "../components/GoogleButton";
+import GoogleButton from "../components/GoogleButton";
 import { MdBookmark, MdFindInPage, MdDescription } from "react-icons/md";
-// import screenShotLight from "../assets/img/app-shot-light.jpeg";
-// import screenShotDark from "../assets/img/app-shot-dark.jpeg";
 import SlackButton from "../components/SlackButton";
 import { handleRouteChange } from "../utils/handleRouteChange";
-import Img from "react-image"
+import Img from "react-image";
+import isLoggedIn from '../utils/auth';
 //import GoogleLogin from "react-google-login";
 
 function Landing() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { colorMode } = useColorMode();
-
+  
   useEffect(() => {
     initGA(config.googleAnalytics.apiKey);
     PageView();
-    if (typeof window !== "undefined" && window.localStorage.getItem("token")) {
-      setIsLoggedIn(true);
-    } else setIsLoggedIn(false);
-  }, [isLoggedIn]);
+  }, []);
 
   return (
     <ThemeProvider>
@@ -54,7 +49,7 @@ function Landing() {
           justifyContent="space-around"
         >
           <Box minWidth="340px" w="30%" as="section" pt={["40", "40", "30", "40"]} pb={50}>
-            <Header landing={true} isLoggedIn={isLoggedIn} />
+            <Header landing={true} isLoggedIn={isLoggedIn()} />
             <Container>
               <Box maxW="xl" mx="auto" px="10px" textAlign="center">
                 <Heading
@@ -79,7 +74,7 @@ function Landing() {
                 </Text>
 
                 <Box mt="6">
-                  {isLoggedIn && (
+                  {isLoggedIn() && (
                     <Button
                       mr="10px"
                       size="lg"
@@ -91,7 +86,7 @@ function Landing() {
                       Browse my Snippets
                     </Button>
                   )}
-                  {!isLoggedIn && (
+                  {!isLoggedIn() && (
                     <Stack
                       mx="3px"
                       spacing={4}
@@ -108,7 +103,7 @@ function Landing() {
                       </Button>
                       <RequestAccess isOpen={isOpen} onClose={onClose} />
                       <SlackButton />
-                      {/* <GoogleButton /> */}
+                      <GoogleButton />
                     </Stack>
                   )}
                 </Box>
@@ -117,19 +112,6 @@ function Landing() {
           </Box>
 
           <Container maxWidth="800px" mx="20px">
-            {/* <Image
-              mt={["0px", "0px", "0px", "50px"]}
-              minWidth="340px"
-              w={["90%", "90%", "90%", "100%"]}
-              maxW="1280px"
-              borderRadius="5px"
-              src={colorMode === "light" ? screenShotLight : screenShotDark}
-              opacity={colorMode === "dark" ? "0.7" : "1"}
-              alt="Codelify app screenshot"
-              style={{
-                animation: "float 6s ease-in-out infinite",
-              }}
-            /> */}
             <Img 
               src={colorMode === "light" ? "https://res.cloudinary.com/codelify/image/upload/f_auto,q_auto/v1583157283/app-shot-light_lp32ha.png" : "https://res.cloudinary.com/codelify/image/upload/f_auto,q_auto/v1583157270/app-shot-dark_jfiiqe.png"} 
               style={{
