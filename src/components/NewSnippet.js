6b8af3c4d5f1cefda8f371929b37095e751580e1
Haja <haja.andri@gmail.com>
2020-03-25 16:11:37 +0100

    set CodeLangage as standalone component

--- src/components/NewSnippet.js
+++ src/components/NewSnippet.js
@@ -24,7 +24,6 @@ import {
   FormControl,
   Alert,
   AlertIcon,
-  TagIcon
 } from "@chakra-ui/core";
 import { LiveProvider, LiveEditor } from "react-live";
 import theme from "prism-react-renderer/themes/nightOwl";
@@ -33,13 +32,7 @@ import { useMutation } from "@apollo/react-hooks";
 import { CREATE_SNIPPET } from "../graphql/mutation";
 import { MY_SNIPPETs } from "../graphql/query";
 import { handleRouteChange } from "../utils/handleRouteChange";
-import { ReactComponent as JsIcon } from '../assets/icons/javascript-plain.svg';
-import { ReactComponent as ReactIcon } from '../assets/icons/react-original.svg';
-import { ReactComponent as PythonIcon } from '../assets/icons/python-plain.svg';
-import { ReactComponent as HtmlIcon } from '../assets/icons/html5-original.svg';
-import { ReactComponent as GoIcon } from '../assets/icons/go-plain.svg';
-import { ReactComponent as RubyIcon } from '../assets/icons/ruby-plain.svg';
-import { IoMdRadioButtonOn } from 'react-icons/io'
+import CodeLangageBar from './CodeLangageBar'
 
 const NewSnippet = props => {
   const { isOpen, onClose, firstField, btnRef, size } = props;
@@ -386,37 +379,7 @@ const formatCurrency = new Intl.NumberFormat("en-US",{
                 code={code.trim()}
               >
                 <FormLabel htmlFor="desc">Code</FormLabel>
-                <Box py="5px" style={{whiteSpace:"nowrap", overflow:"auto"}}>
-                      <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variantColor={codeLangage === "javascript" ? "orange" : "gray"} id="javascript" onClick={langageSelection}  >
-                        <JsIcon style={{width:"20px", height:"auto"}} />
-                        <TagLabel mx="5px">Javascript</TagLabel>
-                      </Tag>
-                      <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variantColor={codeLangage === "python" ? "orange" : "gray"} id="python" onClick={langageSelection}  >
-                        <PythonIcon style={{width:"20px", height:"auto"}} />
-                        <TagLabel mx="5px">Python</TagLabel>
-                      </Tag>
-                      <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variantColor={codeLangage === "jsx" ? "orange" : "gray"} id="jsx" onClick={langageSelection} >
-                        <ReactIcon style={{width:"20px", height:"auto"}} />
-                        <TagLabel mx="5px">React</TagLabel>
-                      </Tag>
-                      <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variantColor={codeLangage === "html" ? "orange" : "gray"} id="html" onClick={langageSelection}  >
-                        <HtmlIcon style={{width:"20px", height:"auto"}} />
-                        <TagLabel mx="5px">HTML</TagLabel>
-                      </Tag>
-                      <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variantColor={codeLangage === "ruby" ? "orange" : "gray"} id="ruby" onClick={langageSelection}  >
-                        <RubyIcon style={{width:"20px", height:"auto"}} />
-                        <TagLabel mx="5px">Ruby</TagLabel>
-                      </Tag>
-                      <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variantColor={codeLangage === "go" ? "orange" : "gray"} id="go" onClick={langageSelection}  >
-                        <GoIcon style={{width:"20px", height:"auto"}} />
-                        <TagLabel mx="5px">Go</TagLabel>
-                      </Tag>
-                      <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variantColor={codeLangage === "other" ? "orange" : "gray"} id="other" onClick={langageSelection}  >
-                        <TagIcon icon={IoMdRadioButtonOn} />
-                        <TagLabel mx="5px">Other</TagLabel>
-                      </Tag>
-                </Box>
-
+                <CodeLangageBar {...{langageSelection, codeLangage}} />
 
                 <LiveEditor
                   padding={10}
