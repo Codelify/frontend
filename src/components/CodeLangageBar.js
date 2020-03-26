c3cb84a25ad0d360a82ed2cfa86b71c65ad2f24c
Haja <haja.andri@gmail.com>
2020-03-26 13:28:50 +0100

    playing around CodeLangage tag color 1

--- src/components/CodeLangageBar.js
+++ src/components/CodeLangageBar.js
@@ -37,9 +37,9 @@ const CodeLangageBar = ({langageSelection, codeLangage}) => {
                 )
             ) : (
                 !selectionMode && (
-                    <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" variantColor="red" onClick={showLangageOptions}>
+                    <Tag borderWidth="1px" borderColor="transparent" size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" backgroundColor="orange.300" onClick={showLangageOptions}>
                     <DevIcon langage={langage} />
-                    <TagLabel style={{textTransform: "uppercase"}} mx="5px">{langage}</TagLabel>
+                    <TagLabel style={{textTransform: "uppercase", fontSize:"bold"}} mx="5px">{langage}</TagLabel>
                 </Tag>
                 )
             )
@@ -47,27 +47,27 @@ const CodeLangageBar = ({langageSelection, codeLangage}) => {
         {
             selectionMode && (
                 <>
-                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" variantColor={langage === "python" ? "red" : "gray"} id="python" onClick={handleSelection}  >
+                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" backgroundColor={langage === "python" ? "orange.300" : "gray.600"} id="python" onClick={handleSelection}  >
                     <DevIcon langage="python" />
                     <TagLabel mx="5px">Python</TagLabel>
                 </Tag>
-                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" variantColor={langage === "jsx" ? "red" : "gray"} id="jsx" onClick={handleSelection} >
+                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" backgroundColor={langage === "jsx" ? "orange.300" : "gray.600"} id="jsx" onClick={handleSelection} >
                     <DevIcon langage="jsx" />
                     <TagLabel mx="5px">React</TagLabel>
                 </Tag>
-                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" variantColor={langage === "html" ? "red" : "gray"} id="html" onClick={handleSelection}  >
+                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" backgroundColor={langage === "html" ? "orange.300" : "gray.600"} id="html" onClick={handleSelection}  >
                     <DevIcon langage="html" />
                     <TagLabel mx="5px">HTML</TagLabel>
                 </Tag>
-                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" variantColor={langage === "ruby" ? "red" : "gray"} id="ruby" onClick={handleSelection}  >
+                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" backgroundColor={langage === "ruby" ? "orange.300" : "gray.600"} id="ruby" onClick={handleSelection}  >
                     <DevIcon langage="ruby" />
                     <TagLabel mx="5px">Ruby</TagLabel>
                 </Tag>
-                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" variantColor={langage === "go" ? "red" : "gray"} id="go" onClick={handleSelection}  >
+                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" backgroundColor={langage === "go" ? "orange.300" : "gray.600"} id="go" onClick={handleSelection}  >
                     <DevIcon langage="go" />
                     <TagLabel mx="5px">Go</TagLabel>
                 </Tag>
-                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" variantColor={langage === "other" ? "red" : "gray"} id="other" onClick={handleSelection}  >
+                <Tag size="lg" style={{cursor: "pointer"}} mx="5px" variant="solid" backgroundColor={langage === "other" ? "orange.300" : "gray.600"} id="other" onClick={handleSelection}  >
                     <TagIcon icon={IoMdRadioButtonOn} />
                     <TagLabel mx="5px">Other</TagLabel>
                 </Tag>    
--- src/components/DevIcon.js
+++ src/components/DevIcon.js
@@ -5,7 +5,7 @@ import {
 import { ReactComponent as JsIcon } from '../assets/icons/javascript-plain.svg';
 import { ReactComponent as ReactIcon } from '../assets/icons/react-original.svg';
 import { ReactComponent as PythonIcon } from '../assets/icons/python-plain.svg';
-import { ReactComponent as HtmlIcon } from '../assets/icons/html5-original.svg';
+import { ReactComponent as HtmlIcon } from '../assets/icons/html5-plain.svg';
 import { ReactComponent as GoIcon } from '../assets/icons/go-plain.svg';
 import { ReactComponent as RubyIcon } from '../assets/icons/ruby-plain.svg';
 import { IoMdAlert } from 'react-icons/io';
@@ -14,40 +14,40 @@ import { FaQuestionCircle } from 'react-icons/fa';
 const DevIcon = ({langage}) => {
     if(langage === "javascript"){
         return (
-            <JsIcon style={{width:"20px", height:"auto"}} />
+            <JsIcon style={{width:"18px", height:"auto"}} />
         )    
     }
     else if (langage === "python"){
         return (
-            <PythonIcon style={{width:"20px", height:"auto"}} />
+            <PythonIcon style={{width:"18px", height:"auto"}} />
         )    
     }
     else if (langage === "jsx"){
         return (
-            <ReactIcon style={{width:"20px", height:"auto"}} />
+            <ReactIcon style={{width:"18px", height:"auto"}} />
         )    
     }
     else if (langage === "html"){
         return (
-            <HtmlIcon style={{width:"20px", height:"auto"}} />
+            <HtmlIcon style={{width:"18px", height:"auto"}} />
         )    
     }
     else if (langage === "go"){
         return (
-            <GoIcon style={{width:"20px", height:"auto"}} />
+            <GoIcon style={{width:"18px", height:"auto"}} />
         )    
     }
     else if (langage === "ruby"){
         return (
-            <RubyIcon style={{width:"20px", height:"auto"}} />
+            <RubyIcon style={{width:"18px", height:"auto"}} />
         )    
     }
     else if (langage === "undefined"){
         return (
-            <Box as={FaQuestionCircle} size="20px" color="red" />
+            <Box as={FaQuestionCircle} size="18px" color="red" />
         )    
     }
-    else return <Box as={IoMdAlert} size="20px" color="red" />
+    else return <Box as={IoMdAlert} size="18px" color="red" />
 }
 
 export default DevIcon
