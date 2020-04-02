d86df300a7ae46bbe8da1114fd10d30b0edc2199
Sorin Chis <kish.sorin@yahoo.com>
2020-04-02 20:10:31 +0300

    added new path for sitemap

--- src/sitemap-builder.js
+++ src/sitemap-builder.js
@@ -8,7 +8,7 @@ const Sitemap = require("react-router-sitemap").default;
 function generateSitemap() {
   return new Sitemap(router)
     .build("https://codelify.dev")
-    .save("./public/sitemap.xml");
+    .save("./sitemap.xml");
 }
 
 generateSitemap();
